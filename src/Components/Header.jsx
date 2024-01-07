4d53dbd5e39f5c499732088086dc1ae1d3f8f418
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2024-01-07 16:19:56 +0000

    minor adjustments to styles

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -19,7 +19,8 @@ export default function Nav() {
         <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
-              <p className="h-8 w-auto">&lt;DW&gt;</p>
+              {/* <p className="h-8 w-auto">&lt;DW&gt;</p> */}
+              <img src='./src/assets/dw.png'></img>
             </a>
           </div>
           <div className="flex lg:hidden">
