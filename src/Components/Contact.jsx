b26addbe5eb08ec2cb3cbb7be9a7d2b0b22fdb4d
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2024-01-07 16:32:16 +0000

    update imports

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,13 +1,6 @@
-import { useState } from 'react'
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
-import { Switch } from '@headlessui/react'
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
+import React from 'react';
 
 export default function Contact() {
-  const [agreed, setAgreed] = useState(false)
 
   return (
     <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
@@ -19,7 +20,6 @@ export default function Nav() {
         <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
-              {/* <p className="h-8 w-auto">&lt;DW&gt;</p> */}
               <img src='./src/assets/dw.png'></img>
             </a>
           </div>
@@ -56,7 +56,7 @@ export default function Nav() {
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
-              <p className="h-8 w-auto">&lt;DW&gt;</p>
+            <img src='./src/assets/dw.png'></img>
             </a>
               <button
                 type="button"
