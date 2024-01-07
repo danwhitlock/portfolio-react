8e9c57b0342fbe3b80caa353b71638d739b60c77
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2024-01-07 15:21:05 +0000

    update project details

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -10,7 +10,7 @@ export default function Project({project}) {
           {project.categories.map((category) => (
             <span
             key={category}  
-            className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+            className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600"
             >
               {category}
             </span>
@@ -19,13 +19,27 @@ export default function Project({project}) {
         </div>
         <div className="group relative">
           <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-            <a href={project.href}>
               <span className="absolute inset-0" />
               {project.title}
-            </a>
           </h3>
           <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{project.description}</p>
         </div>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+              <a
+                href={project.repo}
+                target="_blank"
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Repository
+              </a>
+              <a
+                href={project.live}
+                target='_blank'
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Live Version
+              </a>
+            </div>
       </article>
     );
 }
